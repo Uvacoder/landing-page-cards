7ed37e0eb20107153c997c99855a83defcfdca4a
Jack LaFond <jack.lafond@icloud.com>
2021-03-30 19:50:57 -0400

    streamticker route

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
                 destination: process.env.INVITE,
                 permanent: true,
             },
+            {
+                source: "/streamticker",
+                destination: "https://discord.com/api/oauth2/authorize?client_id=822117936251928586&permissions=3088&scope=bot",
+                permanent: true
+            }
         ]
     },
     target: "serverless"
